a016610acc6cbcb4f0419203dde3a9edec540225
admin <admin@DESKTOP-Q1VU2P3>
2022-02-22 13:08:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingPractice_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько у вас золота?");
+            int temp_gold;
+            temp_gold = Convert.ToInt32(Console.ReadLine());
+            int crystal_price = 3;
+            Console.WriteLine("Вы можете купить максимум " + temp_gold / crystal_price + " кристалов по цене " + crystal_price + " за штуку.");
+            Console.WriteLine("Сколько хотите купить кристалов?");
+            int buy_crystal;
+            buy_crystal = Convert.ToInt32(Console.ReadLine());
+            int max_buy_crystal = temp_gold / crystal_price;
+            Console.WriteLine("Вы купили " + buy_crystal + " кристалов. У вас осталось " + (temp_gold - buy_crystal * crystal_price) + " золота");
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Xalilov_Saidkamol_Task_02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xalilov_Saidkamol_Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            while (!message.Contains("exit"))
+            {
+                if (message != "exit")
+                {
+                    Console.Write("Введите количество повторов сообщения: ");
+                    int repeat = int.Parse(Console.ReadLine());
+                    for (int i = 0; i < repeat; i++)
+                    {
+                        Console.WriteLine(message);
+                    }
+                    Console.WriteLine("Повторить операцию или exit для выхода");
+                    message = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
